3e8063925d27dd627862113df5026d1c6d449d2e
jacob <pauljacobalcaraz@gmail.com>
2020-11-01 22:44:19 +0800

    Added: about link

--- js/index.js
+++ js/index.js
@@ -1,22 +1,5 @@
-const iconTrigger = document.querySelector('#iconTrigger')
-const icon = document.querySelector('#icon')
-const menuContainer = document.querySelector('.menu-container')
+/* <!-- Carousel --> */
+	$('.carousel').carousel({
+		interval: false, //true for auuto next
+	});
 
-iconTrigger.addEventListener('click', () =>{
-    if(icon.classList.contains('fa-bars')){
-        /* Icon */
-        icon.classList.remove('fa-bars')
-        icon.classList.add('fa-close')
-        /* Menu */
-        menuContainer.classList.add('show')
-        menuContainer.classList.remove('hide')
-    }
-    else{
-        
-        icon.classList.add('fa-bars')
-        icon.classList.remove('fa-close')
-        /* Menu */
-        menuContainer.classList.remove('show')
-        menuContainer.classList.add('hide')
-    }
-})
